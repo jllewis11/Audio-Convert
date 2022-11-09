b0119ca3e579c2d4922b389240c438bd8ee59f8a
Sean Del Castillo <seanbdcastillo@gmail.com>
2022-11-09 00:08:19 -0800

    init testing

--- driver.py
+++ driver.py
@@ -31,12 +31,25 @@ parser.add_argument('-d', '--debug', dest='debug', help='prints debug informatio
 parser.add_argument('-v', '--version', action='version', version=f'IdeaBox %(prog)s {IDEABOX_VERSION}')
 args = parser.parse_args()
 
+power_led = LED(LED_CONSTANTS['POWER_PIN'])
 recording_led = LED(LED_CONSTANTS['REC_PIN'])
+warning_led = LED(LED_CONSTANTS['WARNING_PIN'])
+hat_led = LED(LED_CONSTANTS['HAT_PIN'])
+slot1_led = LED(LED_CONSTANTS['SLOT1_PIN'])
+slot2_led = LED(LED_CONSTANTS['SLOT2_PIN'])
+slot3_led = LED(LED_CONSTANTS['SLOT3_PIN'])
+
+power_button = Button(BUTTON_CONSTANTS['POWER_PIN'])
 recording_button = Button(BUTTON_CONSTANTS['REC_PIN'])
+pause_button = Button(BUTTON_CONSTANTS['PAUSE_PIN'])
+mode_button = Button(BUTTON_CONSTANTS['MODE_PIN'])
+
 now = datetime.datetime.now()
 
-def start_recording():
+def recording():
 	recording_button.when_pressed = None
+	recording_button.wait_for_release()
+
 	recording_led.on()
 
 	# TODO Start Recording
@@ -44,32 +57,26 @@ def start_recording():
 	sleep(0.15)
 
 	if args.debug:
-		print('Device is recording... ')
+		print('Device is recording...')
 		start = time()
-		while True:
-			if recording_button.is_pressed:
-				end_recording(start)
-				break
-	else:
-		while True:
-			if recording_button.is_pressed:
-				end_recording()
-				break
-	return
 
-def end_recording(start=None):
+	recording_button.hold_time = 3
+	if recording_button.is_held:
+		if args.debug:
+			print('Recording paused~')
+
+	recording_button.wait_for_release()
+
 	recording_led.off()
-
-	# TODO End Recording
 
 	if args.debug:
 		end = time()
-		recording_length = end-start
-		print(f'Recording Stopped!\nLength: {round(recording_length, 2)}s')
+		recording_time = end-start
+		print(f'Recording complete! {round(recording_time, 2)}s')
 
-	recording_button.when_pressed = start_recording
+	# TODO End recording
 
-	return
+	listen()
 
 # TODO Functionality to pause current recording and return to recording context
 def pause_recording():
@@ -79,12 +86,16 @@ def pause_recording():
 def edit_mode():
 	pass
 
+# TODO Functionality to make driver wait in listen context, waiting for recording or mode switch
+def listen():
+	recording_button.when_pressed = recording
+
+	pause()
+
 print(f'IdeaBox v{IDEABOX_VERSION} Listening... {now}')
 if args.debug:
 	print('Debug information printing...')
 	print(f'LED PINS: {LED_CONSTANTS}')
 	print(f'BUTTON PINS: {BUTTON_CONSTANTS}')
 
-recording_button.when_pressed = start_recording
-
-pause()
+listen()
